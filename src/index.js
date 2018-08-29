feb0fcfd3e6057f1d2b86d2f9a7a77377b1e121b
Mahmoud Hegazy <m.hegazy1992@gmail.com>
2018-08-29 22:59:18 +0200

    configure service work

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
+
 import { AppRegistry } from "react-native";
 import App from "./App";
+import registerServiceWorker from './registerServiceWorker';
 
 AppRegistry.registerComponent("App", () => App);
 
 AppRegistry.runApplication("App", {
   rootTag: document.getElementById("root")
 });
+/*____________________ add service worker function ___________________*/
+
+registerServiceWorker();
